6f659b4864f5f50aaafedaffb00b019cd33da2a0
dmwcats <dmwcats123@gmail.com>
2023-06-06 16:02:52 -0500

    basic recommendation structure

--- src/pages/result.js
+++ src/pages/result.js
@@ -1,13 +1,16 @@
 import React from "react";
-import '../styles/result.css';
+import "../styles/result.css";
 import PieChart from "../components/PieChart";
-
+import Recommendations from "@/components/Recommendations";
 const result = () => {
   return (
-    <div className="container">
-      <div className="backgroundImage"></div>
-      <h1>Result</h1>
-      <PieChart />
+    <div>
+      <div className="container">
+        <div className="backgroundImage"></div>
+        <h1>Result</h1>
+        <PieChart />
+      </div>
+      <Recommendations />
     </div>
   );
 };
