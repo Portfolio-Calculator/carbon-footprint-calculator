c045986f1a744d407b1a9847dca556c37b2fdd07
Andrew Heim <andrewsheim@gmail.com>
2023-06-09 15:44:12 -0600

    homepage css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,12 +4,11 @@ import "../styles/HomePage.css";
 const HomePage = () => {
   return (
     <div className="container">
-      <div className="backgroundImage"></div>
-      <div className="flower">
-      </div>
+      <div className="backgroundImage" />
+      <div className="flower" />
       <div className="content">
         {/* <h1 className="welcometitle">Welcome to the Carbon Footprint Calculator!</h1> */}
-        <p className="description">
+        <p className="flex justify-center description">
           Take your first step towards a sustainable future.
         </p>
         <Link href="/questionnaire">
