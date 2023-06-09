96ea0dc6ba40960d0c651e545736cf073e160afb
Andrew Heim <andrewsheim@gmail.com>
2023-06-09 14:58:18 -0600

    changing titles of the recommendations

--- public/recommendationJson.js
+++ public/recommendationJson.js
@@ -1,63 +1,50 @@
 export const recommendationJson = {
-  "renewable": {
-    title: "Using Renewable Energy",
-    description:
-      "We appreciate that you are using renewable energy sources like solar or wind power. This is the most environmentally friendly option. We recommend continuing to use renewable energy and exploring ways to further reduce carbon footprint in other areas such as transportation and waste management.",
+  renewable: {
+    title: "Use Renewable Energy",
+    description: "By using renewable energy sources like solar or wind power, you are making an environmentally friendly choice. We recommend continuing to use renewable energy and exploring ways to further reduce your carbon footprint in other areas such as transportation and waste management.",
   },
   coal: {
-    title: "Coal-Fired Power Plants",
-    description:
-      "Your electricity is generated using coal-fired power plants. We recommend considering transitioning to cleaner energy sources such as solar or wind power. Reduce electricity consumption and implement energy-efficient practices.",
+    title: "Transition from Coal-Fired Power Plants",
+    description: "Your electricity is currently generated using coal-fired power plants. We recommend considering a transition to cleaner energy sources such as solar or wind power. Additionally, reducing your electricity consumption and implementing energy-efficient practices can make a significant impact.",
   },
   petroleum: {
-    title: "Petroleum-Based Fuels",
-    description:
-      "Your electricity is generated using petroleum-based fuels. We recommend exploring alternative energy sources like solar, wind, or geothermal power. Transitioning to cleaner energy options can significantly reduce carbon emissions.",
+    title: "Switch from Petroleum-Based Fuels",
+    description: "Your electricity is currently generated using petroleum-based fuels. We recommend exploring alternative energy sources like solar, wind, or geothermal power. Transitioning to cleaner energy options can significantly reduce carbon emissions and contribute to a greener future.",
   },
   naturalGas: {
-    title: "Natural Gas Power Plants",
-    description:
-      "Your electricity is generated using natural gas power plants. While natural gas is cleaner than coal or petroleum, it still contributes to greenhouse gas emissions. We suggest exploring renewable energy sources like solar or wind power as a greener alternative.",
+    title: "Explore Greener Alternatives to Natural Gas Power Plants",
+    description: "Your electricity is currently generated using natural gas power plants. While natural gas is cleaner than coal or petroleum, it still contributes to greenhouse gas emissions. We suggest exploring renewable energy sources like solar or wind power as a greener alternative to further reduce your environmental impact.",
   },
   dontKnow: {
-    title: "Unknown Electricity Source",
-    description:
-      "You have a lack of information about the source of electricity. In this case, we recommend contacting the electricity provider or conducting research to determine the source of the electricity. Once the information is available, follow the appropriate recommendations based on the actual electricSource.",
+    title: "Determine the Source of Your Electricity",
+    description: "If you lack information about the source of your electricity, we recommend contacting your electricity provider or conducting research to determine the source. Once the information is available, follow the appropriate recommendations based on the actual electricity source.",
   },
   gas: {
-    title: "Gas-Powered Vehicle",
-    description:
-      "You are currently using a gas-powered vehicle. We recommend considering a more fuel-efficient vehicle or carpooling to reduce gas consumption. Plan your trips efficiently and combine errands to minimize unnecessary driving. Regularly maintain your vehicle to ensure it operates at its optimal efficiency.",
+    title: "Reduce Gas Consumption",
+    description: "You are currently using a gas-powered vehicle. To reduce your gas consumption and minimize your carbon footprint, we recommend considering a more fuel-efficient vehicle or exploring carpooling options. Plan your trips efficiently, combine errands, and ensure regular maintenance of your vehicle for optimal efficiency.",
   },
   hybrid: {
-    title: "Hybrid Vehicle",
-    description:
-      "You are currently using a hybrid vehicle. We recommend continuing to use it as it is a more environmentally friendly option compared to traditional gas-powered vehicles. Consider driving in eco-friendly modes and optimizing your driving habits to maximize fuel efficiency. Explore options to further reduce your carbon footprint, such as using public transportation or cycling for shorter trips.",
+    title: "Continue Using a Hybrid Vehicle",
+    description: "You are currently using a hybrid vehicle, which is a more environmentally friendly option compared to traditional gas-powered vehicles. Consider driving in eco-friendly modes and optimizing your driving habits to maximize fuel efficiency. Exploring additional options like using public transportation or cycling for shorter trips can further reduce your carbon footprint.",
   },
   electric: {
-    title: "Electric Vehicle",
-    description:
-      "You are currently using an electric vehicle. We recommend continuing to use it as it produces zero tailpipe emissions. Ensure you charge your electric vehicle using renewable energy sources for a greener charging process. Install home charging stations or take advantage of public charging infrastructure to conveniently charge your electric vehicle.",
+    title: "Continue Using an Electric Vehicle",
+    description: "You are currently using an electric vehicle, which produces zero tailpipe emissions. We recommend continuing to use it and ensuring that you charge your electric vehicle using renewable energy sources for a greener charging process. Installing home charging stations or utilizing public charging infrastructure can provide convenient charging options.",
   },
-  
   electricPercentage: {
-    title: "Electricity Usage",
-    description:
-      "Switch to energy-efficient appliances, consider installing solar panels or utilizing renewable energy sources, and reduce unnecessary electricity usage. Turn off lights, electronics, and appliances when not in use, and opt for energy-saving settings on devices. Be mindful of heating and cooling usage by adjusting thermostat settings and using insulation techniques to conserve energy.",
+    title: "Reduce Electricity Usage",
+    description: "To reduce your electricity usage and lower your environmental impact, consider switching to energy-efficient appliances, installing solar panels or utilizing renewable energy sources, and reducing unnecessary electricity usage. Simple actions like turning off lights, electronics, and appliances when not in use can make a significant difference.",
   },
   gasPercentage: {
-    title: "Gas Emissions",
-    description:
-      "Practice fuel-efficient driving techniques such as maintaining a steady speed, avoiding aggressive acceleration and braking, and reducing idle time. Explore the possibility of using alternative transportation methods for certain trips, such as public transportation, carpooling, biking, or walking. Combine multiple errands into a single trip to minimize unnecessary driving. Use navigation apps or websites to find the most efficient routes and avoid traffic congestion, further optimizing your gas usage.",
+    title: "Optimize Gas Usage",
+    description: "To optimize your gas usage and minimize emissions, practice fuel-efficient driving techniques such as maintaining a steady speed, avoiding aggressive acceleration and braking, and reducing idle time. Explore alternative transportation methods like public transportation, carpooling, biking, or walking for certain trips. Plan your routes efficiently to avoid traffic congestion and further optimize your gas usage.",
   },
   flightPercentage: {
-    title: "Air Travel Emissions",
-    description:
-      "Consider alternative modes of transportation for shorter trips, such as train or bus, which generally have lower carbon emissions. When flying is necessary, choose direct flights whenever possible as takeoffs and landings contribute to a significant portion of carbon emissions. Opt for airlines that have a better environmental track record and prioritize fuel-efficient aircraft.",
+    title: "Minimize Air Travel Emissions",
+    description: "Consider alternative modes of transportation for shorter trips, such as train or bus, which generally have lower carbon emissions. When flying is necessary, choose direct flights whenever possible as takeoffs and landings contribute to a significant portion of carbon emissions. Opt for airlines that have a better environmental track record and prioritize fuel-efficient aircraft. Additionally, you can offset your air travel emissions by supporting reputable carbon offset projects.",
   },
   recycle: {
-    title: "Recycling",
-    description:
-      "Start recycling to reduce waste and minimize your environmental impact. Educate yourself about the benefits of recycling and how to properly sort and dispose of recyclable materials. Support local recycling programs and initiatives to contribute to a more sustainable community.",
+    title: "Start Recycling",
+    description: "By starting to recycle, you can reduce waste and minimize your environmental impact. Educate yourself about the benefits of recycling and learn how to properly sort and dispose of recyclable materials. Support local recycling programs and initiatives to contribute to a more sustainable community. Remember to reduce and reuse before recycling whenever possible.",
   },
 };
