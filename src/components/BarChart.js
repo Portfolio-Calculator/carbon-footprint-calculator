83c27c4def2965bbdef5f3319167d2b3ef8bcc69
Andrew Heim <andrewsheim@gmail.com>
2023-06-09 15:35:21 -0600

    adding bar chart w/ some formatting

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -22,34 +22,9 @@ ChartJS.register(
 
 export default function BarChart({ data }) {
 
-  // const data = {
-  //   labels: [
-  //     "Electric Emissions",
-  //     "Vehicle Usage",
-  //     "Flights",
-  //     "Recycle",
-  //     "Gas Emissions",
-  //   ],
-  //   datasets: [
-  //     {
-  //       label: "Tonnes of CO2",
-  //       data: [
-  //         0.975,
-  //       ],
-  //       backgroundColor: [
-  //         "rgba(255, 99, 132)",
-  //         "rgba(54, 162, 235)",
-  //         "rgba(255, 206, 86)",
-  //         "rgba(75, 192, 192)",
-  //         "rgba(153, 102, 255)",
-  //       ],
-  //     },
-  //   ],
-  // };
-
   return (
-    <div>
-      <Bar data={data} />;
+    <div style={{ width: "40%", height: "auto" }}>
+      <Bar data={data} />
       {/* <Bar data={data} /> */}
     </div>
   );
--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -7,7 +7,7 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function PieChart({ data }) {
   return (
-    <div style={{ width: "30%", height: "auto" }}>
+    <div style={{ width: "40%", height: "auto" }}>
       <Pie data={data} />
     </div>
   );
