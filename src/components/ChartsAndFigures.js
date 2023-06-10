8fe57418404dbcf334436a0e17f217b2cec752bc
Divya <dm.divya.mahajan@gmail.com>
2023-06-09 17:44:03 -0700

    catorise

--- src/components/ChartsAndFigures.js
+++ src/components/ChartsAndFigures.js
@@ -3,6 +3,16 @@ import PieChart from "./PieChart";
 import BarChart from "./BarChart";
 import "../styles/ChartsAndFigures.css";
 const ChartsAndFigures = ({ data, total }) => {
+  let footprintCategory = "";
+  if (total >= 24) {
+    footprintCategory = "High Carbon Footprint";
+  } else if (total >= 16) {
+    footprintCategory = "Medium Carbon Footprint";
+  } else {
+    footprintCategory = "Low Carbon Footprint";
+  }
+
+  console.log("footprintCategory", footprintCategory);
   return (
     <div className="container">
       <h2 className="text-3xl font-bold mb-4 -mt-20 text-center">
@@ -13,19 +23,19 @@ const ChartsAndFigures = ({ data, total }) => {
         Your annual CO2 emissions amount to <strong>{total.toFixed(2)}</strong>{" "}
         metric tonnes
       </div>
+
+      <p className="text-center">
+        Based on your emissions, you are categorized as having a{" "}
+        <strong>{footprintCategory}</strong> .
+      </p>
       <ul>
         <li className="font-semibold mb-2">
           Low Carbon Footprint: 6.00 to 15.99
-          {/* Low Carbon Footprint: 0 to 2 metric tonnes per year */}
         </li>
         <li className="font-semibold mb-2">
           Medium Carbon Footprint: 16.00 to 23.99
-          {/* Medium Carbon Footprint: 2 to 5 metric tonnes per year */}
         </li>
-        <li className="font-semibold">
-          High Carbon Footprint: 24.00 to 31.99
-          {/* High Carbon Footprint: more than 5 metric tonnes per year */}
-        </li>
+        <li className="font-semibold">High Carbon Footprint: 24.00 to 31.99</li>
       </ul>
       <BarChart data={data} />
     </div>
