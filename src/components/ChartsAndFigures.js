6edd62fe0d881670479b2afc158edba66e1702a7
Divya <dm.divya.mahajan@gmail.com>
2023-06-09 17:18:55 -0700

    css Charts

--- src/components/ChartsAndFigures.js
+++ src/components/ChartsAndFigures.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React from "react";
 import PieChart from "./PieChart";
 import BarChart from "./BarChart";
-
+import "../styles/ChartsAndFigures.css";
 const ChartsAndFigures = ({ data, total }) => {
   return (
     <div className="container">
-      <h2 className="text-3xl font-bold mb-4 -mt-20 text-center">Your Carbon Footprint</h2>
-      <div className="mb-6 text-center"> {total} (CO2 emissions in tonnes per year)</div>
-      <ul className="mb-8">
-        <li className="font-semibold mb-2">Low Carbon Footprint: 6,000 to 15,999</li>
-        <li className="font-semibold mb-2">Medium Carbon Footprint: 16,000 to 23,999</li>
-        <li className="font-semibold">High Carbon Footprint: 24,000 to 31,999</li>
-      </ul>
+      <h2 className="text-3xl font-bold mb-4 -mt-20 text-center">
+        Your Carbon Footprint
+      </h2>
       <PieChart data={data} />
+      <div className="mb-6 text-center">
+        Your annual CO2 emissions amount to <strong>{total.toFixed(2)}</strong>{" "}
+        metric tonnes
+      </div>
+      <ul>
+        <li className="font-semibold mb-2">
+          Low Carbon Footprint: 6.00 to 15.99
+          {/* Low Carbon Footprint: 0 to 2 metric tonnes per year */}
+        </li>
+        <li className="font-semibold mb-2">
+          Medium Carbon Footprint: 16.00 to 23.99
+          {/* Medium Carbon Footprint: 2 to 5 metric tonnes per year */}
+        </li>
+        <li className="font-semibold">
+          High Carbon Footprint: 24.00 to 31.99
+          {/* High Carbon Footprint: more than 5 metric tonnes per year */}
+        </li>
+      </ul>
       <BarChart data={data} />
     </div>
   );
 };
-
 
 export default ChartsAndFigures;
